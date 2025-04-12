16540b23e4c3abe35382765499e3233025780659
manasdekivadia <153750149+manasdekivadia@users.noreply.github.com>
2025-04-12 19:35:40 +0530

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -11,7 +11,7 @@ export const Banner = () => {
   const [text, setText] = useState('');
   const [delta, setDelta] = useState(300 - Math.random() * 100);
   const [index, setIndex] = useState(1);
-  const toRotate = [ "Web Developer", "Web Designer", "UI/UX Designer" ];
+  const toRotate = [ "Cyber Security Analayst", "Competitive Programmer", "AI/ML Engineer", "UI/UX Designer"];
   const period = 2000;
 
   useEffect(() => {
@@ -57,7 +57,7 @@ export const Banner = () => {
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                 <span className="tagline">Welcome to My Portfolio</span>
                 <h1>{`Hi! I'm Manas`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "AI/ML Engineer", "CyberSecurity Expert" ]'><span className="wrap">{text}</span></span></h1>
-                  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                  <p>Hi, I’m a passionate Web Developer, Cybersecurity Enthusiast, AI/ML Engineer, and Software Developer dedicated to building innovative, secure, and intelligent digital solutions. With a strong foundation in full-stack development, I craft seamless user experiences and robust backend systems. My fascination with cybersecurity drives me to create applications that are not just functional but resilient. I thrive at the intersection of software engineering and artificial intelligence, constantly exploring how smart technologies can solve real-world problems. Whether it's developing sleek web apps, training machine learning models, or strengthening digital defenses, I’m always excited to push boundaries and learn something new.</p>
                   <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
               </div>}
             </TrackVisibility>
