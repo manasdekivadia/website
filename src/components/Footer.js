0d888f54f3e162de881701b62bc11423e9ee4307
manasdekivadia <153750149+manasdekivadia@users.noreply.github.com>
2025-04-12 19:41:10 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -16,11 +16,11 @@ export const Footer = () => {
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="#"><img src={navIcon1} alt="Icon" /></a>
-              <a href="#"><img src={navIcon2} alt="Icon" /></a>
-              <a href="#"><img src={navIcon3} alt="Icon" /></a>
+              <a href="https://www.linkedin.com/in/manas-dekivadia-497437287/"><img src={navIcon1} alt="Icon" /></a>
+              <a href="mailto:manasdekivadia1729@gmail.com"><img src={navIcon2} alt="Icon" /></a>
+              <a href="https://www.instagram.com/manasay_what/"><img src={navIcon3} alt="Icon" /></a>
             </div>
-            <p>Copyright 2022. All Rights Reserved</p>
+            <p>Copyright 2025. All Rights Reserved</p>
           </Col>
         </Row>
       </Container>
