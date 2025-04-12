fabc3ef5c23ccb910ad578834c4b79d28d00351b
manasdekivadia <153750149+manasdekivadia@users.noreply.github.com>
2025-04-12 19:51:14 +0530

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -11,8 +11,8 @@ export const Projects = () => {
 
   const projects = [
     {
-      title: "Business Startup",
-      description: "Design & Development",
+      title: "Honey Trap",
+      description: " Honeypot For SSH & HTML",
       imgUrl: projImg1,
     },
     {
@@ -51,7 +51,7 @@ export const Projects = () => {
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
                 <h2>Projects</h2>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                <p>Here are some of the projects I’ve worked on — from real-world applications to personal experiments. Each one showcases my skills in design, development, and problem-solving.</p>
                 <Tab.Container id="projects-tabs" defaultActiveKey="first">
                   <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
                     <Nav.Item>
